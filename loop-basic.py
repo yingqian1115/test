bc380f402db8fdb441f22d6adb295156cf5c6c1f
Wang-yingqian <yingqianwng1115@gmail.com>
2022-05-03 19:52:37 +0800

    First Commit

---
+++ loop-basic.py
@@ -0,0 +1,16 @@
+# while迴圈
+# 1+2+3+...10
+# n=1
+# sum=0 #紀錄累加的結果
+# while n<=10:
+ # sum+=n
+ # print(n)
+ # n+=1
+# print(sum)
+
+# for迴圈
+# 1+2+3+...10
+n=0
+for x in range(11):
+    n+=x
+print(n)   
